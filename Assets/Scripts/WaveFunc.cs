1a1d8ed0065017fdb8480d1a3d6fe52963a47a4e
Vergillus <merty89@hotmail.com>
2017-06-09 16:36:53 +0300

    Implementation

--- Assets/Scripts/WaveFunc.cs
+++ Assets/Scripts/WaveFunc.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class WaveFunc : MonoBehaviour {
 
-    [SerializeField, Range(1.0f,20.0f)]
-    private float Wavelenght = 2.0f;
+    //[SerializeField, Range(1.0f, 20.0f)]
+    private float[] Wavelenght;
 
-    [SerializeField ,Range(1.0f, 10.0f)]
-    private float Speed = 5.0f;
+    //[SerializeField, Range(1.0f, 10.0f)]
+    private float[] Speed;
+
+    [SerializeField, Range(0.0f, 1.0f)]
+    private float steepnessParameter = 0.5f;
+    [SerializeField, Range(1.0f, 5.0f)]
+    private int numWaves = 1;
 
     Renderer Rend;
     Mesh Mesh;
@@ -18,8 +23,8 @@ public class WaveFunc : MonoBehaviour {
     Vector3[] Direction;
 
     int Index = 0;
-    float Frequency;
-    float PhaseConstant;    
+    float[] Frequency;
+    float[] PhaseConstant;    
     float ElapsedTime;
     float[] Steepness;
     float[] Amplitute;
@@ -32,12 +37,16 @@ public class WaveFunc : MonoBehaviour {
 
         ElapsedTime = 0;
 
-        Frequency = Mathf.Sqrt(((2 * Mathf.PI) / Wavelenght) * Physics.gravity.y * -1);
+        //Frequency = Mathf.Sqrt(((2 * Mathf.PI) / Wavelenght) * Physics.gravity.y * -1);
         //Frequency = 2 / Wavelenght;        
-        PhaseConstant = Speed * Frequency;        
+        //PhaseConstant = Speed * Frequency;        
 
         Vertices = Mesh.vertices;
 
+        Wavelenght = new float[Vertices.Length];
+        Speed = new float[Vertices.Length];
+        Frequency = new float[Vertices.Length];
+        PhaseConstant = new float[Vertices.Length];
         Amplitute = new float[Vertices.Length];
         Steepness = new float[Vertices.Length];
         Direction = new Vector3[Vertices.Length];
@@ -45,9 +54,15 @@ public class WaveFunc : MonoBehaviour {
 
         for (int i = 0; i < Vertices.Length; i++)
         {
-            Amplitute[i] = Random.Range(0.0f, 10.0f);
-            Steepness[i] = 1 / (Wavelenght * Amplitute[i]);
-            Direction[i] = new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            Wavelenght[i] = Random.Range(10.0f, 50.0f);
+            Speed[i] = Random.Range(1.0f, 20.0f);
+
+            Frequency[i] = Mathf.Sqrt(((2 * Mathf.PI) / Wavelenght[i]) * Physics.gravity.y * -1);
+            PhaseConstant[i] = Speed[i] * Frequency[i];
+            Amplitute[i] = Random.Range(1.0f, 50.0f);
+            Steepness[i] = Mathf.Clamp(steepnessParameter / (Wavelenght[i] * Amplitute[i] * 1),0.0f,1.0f);
+            
+            Direction[i] = new Vector3(Random.Range(1.0f, 1.0f), Random.Range(0.0f, 0.0f), Random.Range(1.0f, 1.0f));
         }
 
 	}
@@ -75,12 +90,12 @@ public class WaveFunc : MonoBehaviour {
 
             //T += Mathf.Cos(Vector3.Dot(Direction[Index], Vertices[Index]) * Frequency + ElapsedTime * PhaseConstant);
 
-            float waveY = Steepness[Index] * Amplitute[Index] * Direction[Index].y * Mathf.Cos(Vector3.Dot(Direction[Index], Vertices[Index]) * Frequency + ElapsedTime * PhaseConstant);
-            float waveX = Steepness[Index] * Amplitute[Index] * Direction[Index].x * Mathf.Cos(Vector3.Dot(Direction[Index], Vertices[Index]) * Frequency + ElapsedTime * PhaseConstant);
-            
-            waveY += waveY + waveX + waveX;
-            Vertices[Index].y = waveY;
-            //VertexColors[Index] = new Color(waveY, waveY, waveY) + Rend.material.color;            
+            float waveY = Steepness[Index] * Amplitute[Index] * Direction[Index].y * Mathf.Cos(Vector3.Dot(Direction[Index], Vertices[Index]) * Frequency[Index] + ElapsedTime * PhaseConstant[Index]);
+            float waveX = Steepness[Index] * Amplitute[Index] * Direction[Index].x * Mathf.Cos(Vector3.Dot(Direction[Index], Vertices[Index]) * Frequency[Index] + ElapsedTime * PhaseConstant[Index]);
+                        
+            waveY += waveY + waveX + waveX;            
+            Vertices[Index].y = waveY;            
+            //VertexColors[Index] = new Color(Vertices[Index].y, Vertices[Index].y, Vertices[Index].y) + Rend.material.color;            
             Index++;
         }
 
